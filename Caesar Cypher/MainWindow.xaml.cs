d51fadc05495f06aece9b2a9b0bdaf9034b537bc
Cregennan <silver2prod@gmail.com>
2022-02-15 02:58:57 +0300

    Добавьте файлы проекта.

---
+++ Caesar Cypher/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using SourceChord.FluentWPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Caesar_Cypher
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (var s in Service.Operations)
+            {
+                OperationComboBox.Items.Add(s);
+            }
+            OperationComboBox.SelectedIndex = 0;
+            foreach(var s in Service.AlphabetDescriptions)
+            {
+                AlphabetComboBox.Items.Add(s);
+            }
+            AlphabetComboBox.SelectedIndex = 0;
+        }
+
+        private void OperationComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var index = OperationComboBox.SelectedIndex;
+            OperationLabel.Content = Service.OperationLabelStates[index];
+            LaunchButton.Content = Service.ButtonStates[index];
+        }
+
+        private void LaunchButton_Click(object sender, RoutedEventArgs e)
+        {
+            String RawShiftValue = Regex.Replace(ShiftValueField.Text, @"\s+", "");
+
+            if (!Service.OnlyNumbers.IsMatch(RawShiftValue))
+            {
+                AcrylicMessageBox.Show(this, "Ошибка в поле сдвига", "Ошибка");
+                return;
+            }
+
+            var index = AlphabetComboBox.SelectedIndex;
+            String RawText = MainText.Text;
+            Regex CurrentAlhpabetRegex = Service.AlphabetRegices[index];
+            String CurrentAlphabet = Service.Alphabets[index];
+            int CurrentAlphabetLength = CurrentAlphabet.Length;
+
+
+            //Понижаем регистр
+            //String FilteredText = RawText.ToLower();
+            ////Убираем пустое место
+            //FilteredText = Regex.Replace(RawText.ToLower(), @"\s+", "");
+            ////Убираем неалфавитные символы
+            String FilteredText = CurrentAlhpabetRegex.Replace(Regex.Replace(RawText.ToLower(), @"\s+", ""), String.Empty);
+
+            if (FilteredText.Length == 0)
+            {
+                MessageBox.Show(this, "В заданном тексте нет символов выбранного алфавита", "Ошибка");
+                return;
+            }
+
+
+
+            int ModdedShiftValue = (int)BigInteger.ModPow(BigInteger.Parse(RawShiftValue), 1, CurrentAlphabetLength);
+            int RealShiftValue = OperationComboBox.SelectedIndex == 0 ? ModdedShiftValue : CurrentAlphabetLength - ModdedShiftValue;
+
+            String result = "";
+            foreach(var c in FilteredText)
+            {
+                result += CurrentAlphabet[(CurrentAlphabet.IndexOf(c) + RealShiftValue) % CurrentAlphabetLength];
+            }
+
+            String ResultWindowTitle = Service.ResultWindowTitles[OperationComboBox.SelectedIndex];
+            (new ResultWindow(result, ResultWindowTitle)).ShowDialog();
+        }
+    }
+}
---
+++ Caesar Cypher/Service.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Caesar_Cypher
+{
+    public class Service
+    {
+        public static readonly String[] OperationLabelStates = {
+            "Текст для шифрования (регистр не учитывается)",
+            "Текст для дешифрования (регистр не учитывается)"
+        };
+        public static readonly String[] Operations = {
+            "Шифрование",
+            "Дешифрование"
+        };
+        public static readonly String[] AlphabetDescriptions = {
+            "Русский",
+            "Английский"
+        };
+        public static readonly String[] ButtonStates = {
+            "Зашифровать",
+            "Расшифровать"
+        };
+        public static readonly String[] ResultWindowTitles = { 
+            "Результат шифрования",
+            "Результат дешифрования"
+        };
+        public static readonly Regex OnlyNumbers = new Regex("^[0-9]+$");
+        public static readonly Regex[] AlphabetRegices =
+        {
+             new Regex("[^а-я0-9]"),
+             new Regex("[^a-z0-9]")
+        };
+        public static readonly String[] Alphabets =
+        {
+            "абвгдеёжзийклмнопрстуфхцчшщъыьэюя0123456789",
+            "abcdefghijklmnopqrstuvwxyz0123456789"
+        };
+    
+    }
+}
